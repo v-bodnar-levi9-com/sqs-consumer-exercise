1966f61772211d8256849f293412fba92b0d6b9a
vbodnar <v.bodnar@levi9.com>
2025-11-07 15:40:10 +0200

    set application to run in one shot command with docker compose

--- .server/run.sh
+++ .server/run.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 
-command -v docker >/dev/null 2>&1 || { echo >&2 "This script requires Docker. Ask your interviewer for an alternative"; exit 1; }
-
-localstack_is_running=$(docker ps | grep localstack | wc -l)
-if [ "$localstack_is_running" == "1" ]; then
-    echo "Localstack is already running."
-else
-    localstack start -d
-fi
-
-echo "Starting event server"
+echo "Starting event server (producer)..."
 ./.server/start.py
--- src/main.py
+++ src/main.py
@@ -1,38 +1,52 @@
 #!/usr/bin/env python
-
 import collections
 import json
+import os
 import localstack_client.session as boto3
 
 client = boto3.client('sqs')
 
-queue = client.list_queues()["QueueUrls"][0]
+QUEUE_NAME = os.getenv("SQS_QUEUE_NAME", "hands-on-interview")
+
+def get_queue_url(queue_name):
+    try:
+        res = client.get_queue_url(QueueName=queue_name)
+    except:
+        res = client.create_queue(QueueName=queue_name)
+    return res["QueueUrl"]
+
+queue_url = get_queue_url(QUEUE_NAME)
 
 
 event_counts = collections.defaultdict(float)
 event_sums = collections.defaultdict(float)
 
-for i in range(100):
-    response = client.receive_message(QueueUrl=queue, MaxNumberOfMessages=10)
+while True:  # TODO: Replace with proper loop and exit conditions
+    print("Fetching messages from queue_url...") # TODO use logging
+    response = client.receive_message(
+        QueueUrl=queue_url, 
+        MaxNumberOfMessages=10,
+        WaitTimeSeconds=20 # <-- SQS Long Polling. Це ефективна заміна 'sleep'
+    )
     if 'Messages' not in response:
-        break
+        continue
     for message in response['Messages']:
         body = message['Body']
         body = json.loads(body)
         if 'type' not in body or 'value' not in body:
-            client.delete_message(QueueUrl=queue, ReceiptHandle=message['ReceiptHandle'])
+            client.delete_message(QueueUrl=queue_url, ReceiptHandle=message['ReceiptHandle'])
             continue
         message_type = body['type']
         try:
             message_value = float(body['value'])
         except:
-            client.delete_message(QueueUrl=queue, ReceiptHandle=message['ReceiptHandle'])
+            client.delete_message(QueueUrl=queue_url, ReceiptHandle=message['ReceiptHandle'])
             continue
         event_counts[message_type] += 1
         event_sums[message_type] += message_value
-        client.delete_message(QueueUrl=queue, ReceiptHandle=message['ReceiptHandle'])
+        client.delete_message(QueueUrl=queue_url, ReceiptHandle=message['ReceiptHandle'])
 
-for key in event_counts:
-    print('Event type:', key)
-    print('Count:', event_counts[key])
-    print('Sum:', event_sums[key])
+    for key in event_counts:
+        print('Event type:', key)
+        print('Count:', event_counts[key])
+        print('Sum:', event_sums[key])
